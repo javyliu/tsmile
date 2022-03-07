10d0c583a6474f1d85cad977f91e3fe56f2d0c79
qmliu <qmliu@pearlinpalm.com>
2022-03-07 13:33:24 +0800

    update database setting

--- config/database.js
+++ config/database.js
@@ -1,11 +1,7 @@
 require('dotenv').config();
 module.exports = {
   "development": {
-    "username": "javy",
-    "password": process.env.tsmile_pwd,
-    "database": "tsmile",
-    "host": "192.168.30.33", 
-    "port": "3306",
+    "url": process.env.db,
     "dialect": "mysql"
   },
   "test": {
@@ -16,11 +12,7 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "javy",
-    "password": process.env.tsmile_pwd,
-    "database": "tsmile",
-    "host": "10.0.224.9", 
-    "port": "22679",
+    "url": process.env.db_pro,
     "dialect": "mysql"
   }
 }
